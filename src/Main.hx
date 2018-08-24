09ccab9b53044e277a808a9b6609df507917038c
Bin Xin <rhyzix@gmail.com>
2018-08-24 14:06:16 +0800

    first commit

---
+++ src/Main.hx
@@ -0,0 +1,13 @@
+// noop
+class Main { static function main() { } }
+
+@:expose('hxd')
+class Hxd {
+  static var App = hxd.App;
+}
+
+@:expose('h2d')
+class H2d {
+  static var Tile = h2d.Tile;
+  static var Bitmap = h2d.Bitmap;
+}
